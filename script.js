4cd4a7357c973d43614c9576837f8b16c09939c0
Aditya Sahani <166254189+adityasahani001@users.noreply.github.com>
2025-05-12 22:27:49 +0530

    script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const listContainer = document.getElementById("list-container");
 function addTask(){
     if(inputBox.value === ''){
         alert("You must write something!");
-
     }
     else{
         let li = document.createElement("li");
