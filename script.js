9d480d9dbef63b49a18c55779aa9856eb5e42cc3
Aditya Sahani <166254189+adityasahani001@users.noreply.github.com>
2025-05-17 23:25:17 +0530

    script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function addTask(){
     if(inputBox.value === ''){
         alert("You must write something!");
     }
+
     else{
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
@@ -12,7 +13,6 @@ function addTask(){
         span.innerHTML="\u00d7";
         li.appendChild(span);
     }
-
     inputBox.value="";
     saveData();
 }
