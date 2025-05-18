86ad3c4318bf7bb248d065dc3b0e6aa0c7397705
Aditya Sahani <166254189+adityasahani001@users.noreply.github.com>
2025-05-18 23:42:51 +0530

    script.js

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ function addTask(){
     inputBox.value="";
     saveData();
 }
-
 listContainer.addEventListener("click",function(e){
     if(e.target.tagName == "LI"){
         e.target.classList.toggle("checked");
